4640889f8d983001a4032b32c922d10c8d83c496
Abhi5453Abhi <40296575+Abhi5453Abhi@users.noreply.github.com>
2018-10-30 20:20:15 +0530

    Create CamelCase.js

---
+++ CamelCase.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the camelcase function below.
+function camelcase(s) {
+    var n=s.length,sum=0;
+    let i;
+    for(i=1;i<n;i++)
+        {
+            if(s[i]>="A"&&s[i]<="Z")sum++;
+        }
+    sum++;
+    return sum;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    let result = camelcase(s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
